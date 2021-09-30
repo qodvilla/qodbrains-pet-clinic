b974b0642ae94a4d81483674384c6e3bf55c057e
--replace-all <villa@qodbrains.com>
2021-09-30 20:27:44 +0200

    Project initialization

---
+++ src/main/java/com/qodbrains/qodbrainspetclinic/QodbrainsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.qodbrains.qodbrainspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodbrainsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QodbrainsPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qodbrains/qodbrainspetclinic/QodbrainsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qodbrains.qodbrainspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodbrainsPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
