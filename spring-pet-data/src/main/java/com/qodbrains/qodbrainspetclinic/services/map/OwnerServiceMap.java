48fb60a2c976f1a6289aaa7d547ea31ee6827cf6
--replace-all <villa@qodbrains.com>
2021-10-20 14:49:55 +0200

    Implemented Map Based Services

---
+++ spring-pet-data/src/main/java/com/qodbrains/qodbrainspetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package com.qodbrains.qodbrainspetclinic.services.map;
+
+import com.qodbrains.qodbrainspetclinic.model.Owner;
+import com.qodbrains.qodbrainspetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+}
---
+++ spring-pet-data/src/main/java/com/qodbrains/qodbrainspetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.qodbrains.qodbrainspetclinic.services.map;
+
+import com.qodbrains.qodbrainspetclinic.model.Vet;
+import com.qodbrains.qodbrainspetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
